af01484c07d85dcd2917aabae60098673ffcd7a9
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-09-16 07:28:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -31,11 +31,12 @@
             <li class="nav-item">
           <a class="nav-link" href="get-result.php">Get Result</a>
         </li>
+      </ul>  
     </div>
   </div>
 </nav>
     <h1>Hello, world!</h1>
-  <div class="container">
+  </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
